30e94deb07faf4812566e325e0c56d238848a035
Avinash Pancham <avinashpancham@outlook.com>
2020-08-25 23:16:06 +0200

    Switch to Gorilla and add mainpage

--- url_shortener/main.go
+++ url_shortener/main.go
@@ -7,24 +7,26 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"text/template"
 
 	utils "./utils"
+	"github.com/gorilla/mux"
 )
 
 var bucketName = "urlShortener"
 var db = utils.CreateDB(bucketName)
 
 func shortenHandler(w http.ResponseWriter, r *http.Request) {
-	url := r.URL.Path[len("/shortener/"):]
+	vars := mux.Vars(r)
 
 	// Get hash value of url as shortened url
-	hashValue := fmt.Sprintf("%x", sha1.Sum([]byte(url)))
+	hashValue := fmt.Sprintf("%x", sha1.Sum([]byte(vars["page"])))
 	trimmedHashValue := strings.ReplaceAll(hashValue, " ", "")[:5]
 
 	// Store and return response
-	mappedReturn, _ := json.Marshal(map[string]string{url: trimmedHashValue})
+	mappedReturn, _ := json.Marshal(map[string]string{vars["page"]: trimmedHashValue})
 	w.Write([]byte(string(mappedReturn)))
-	utils.WriteRecord(db, bucketName, trimmedHashValue, url)
+	utils.WriteRecord(db, bucketName, trimmedHashValue, vars["page"])
 }
 
 func redirectHandler(w http.ResponseWriter, r *http.Request) {
@@ -35,11 +37,24 @@ func redirectHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, redirectURL, 302)
 }
 
+type TodoPageData struct {
+	PageTitle string
+}
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("layout.html"))
+	data := TodoPageData{
+		PageTitle: "URL shortener",
+	}
+	tmpl.Execute(w, data)
+}
+
 func main() {
 	defer db.Close()
-
-	http.HandleFunc("/shortener/", shortenHandler)
-	http.HandleFunc("/", redirectHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/shortener/{page}", shortenHandler)
+	r.HandleFunc("/{hash:[a-z0-9]{5}}", redirectHandler)
+	r.HandleFunc("/", mainHandler)
+	log.Fatal(http.ListenAndServe(":8080", r))
 
 }
