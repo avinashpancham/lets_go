ebaec8be7584156e584cc41475f8fdb7682f20e4
Avinash Pancham <avinashpancham@outlook.com>
2020-10-25 12:58:11 +0100

    Use filename from header or url

--- parallell_downloader/utils/download/download.go
+++ parallell_downloader/utils/download/download.go
@@ -3,10 +3,10 @@ package download
 import (
 		"fmt"
 		"io"
-		"math/rand"
+		"log"
 		"net/http"
 		"os"
-		"strconv"
+		"strings"
 	)
 
 // Statistics is a container for download progress statistics
@@ -36,14 +36,52 @@ func (tr *tracker) Write(p []byte) (int, error) {
 	return n, nil
 }
 
+func getFileName(responseHeader http.Header, url string) string {
+	headerName := responseHeader.Get("Content-Disposition")
+
+	var fileName string
+
+	// Check if filename is present in the header else make it from url
+	if headerName == "" {		
+		splitURL := strings.Split(url,"/")
+		fileName= splitURL[len(splitURL)-1] 
+	} else {
+		fileName = headerName 
+	}
+
+	// Check if filename already exists
+	if _, err := os.Stat(fileName); err == nil {
+		// If filename exists try saving with suffix (i)
+		i := 1
+		fileNameComponents := strings.Split(fileName,".")	
+		
+		for {
+			fileName := fmt.Sprintf(fileNameComponents[0] + "(%d)." + fileNameComponents[1], i)
+			if _, err := os.Stat(fileName); os.IsNotExist(err) {
+				// Check whether the form with suffix (i) exists, else increment i
+				return fileName
+			}
+			i++
+		}
+	} else if !os.IsNotExist(err) {
+		// Catch errors due to permission etc
+		log.Fatal(err)	  
+	} 
+	return fileName
+}
+
+
 // Downloader downloads the content from url
 func Downloader(url string, item int, c chan Statistics) {
 	resp, _ := http.Get(url)
 	defer resp.Body.Close()
-	size, _ := strconv.Atoi(resp.Header.Get("Content-Length"))
 
-	name := fmt.Sprintf("test%d.tar.gz", rand.Intn(10000))
-	f, _ := os.Create(name)
+	// Get file info
+	size := int(resp.ContentLength)
+	fileName := getFileName(resp.Header, url)
+
+	// name := fmt.Sprintf("%s%d.%s", fileName,rand.Intn(10000), fileExtension)
+	f, _ := os.Create(fileName)
 	defer f.Close()
 
 	tr := &tracker{size: size, item: item, channel: c}
